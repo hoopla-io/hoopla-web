c4ab378be3fcfd244bd9288bbaaa1a346b6ef320
lastninja294 <lastninja294@gmail.com>
2025-06-11 16:38:06 +0500

    fix: image config

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
+        protocol: "https",
         hostname: "api.hoopla.uz",
         port: "",
         pathname: "/images/**",
