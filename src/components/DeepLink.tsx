60252ea39a55104b63bd31a584cf01ce542a1cfd
lastninja294 <lastninja294@gmail.com>
2025-06-23 10:52:20 +0500

    deeplink

--- src/components/DeepLink.tsx
+++ src/components/DeepLink.tsx
@@ -14,12 +14,14 @@ import Image from "next/image";
 
 import HooplaIcon from "@/public/images/app/hoopla.webp";
 import { usePathname } from "@/i18n/routing";
-import { useLocale } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 
 export default function DeepLink() {
   const pathname = usePathname();
   const locale = useLocale();
   const [open, setOpen] = useState(false);
+
+  const t = useTranslations();
 
   useEffect(() => {
     const shouldOpen = pathname.includes("/shop");
@@ -37,6 +39,8 @@ export default function DeepLink() {
     if (isAndroid) {
       const intentUrl = `intent://${pathname}#Intent;scheme=https;package=uz.alphazet.hoopla;S.browser_fallback_url=${encodedFallback};end`;
       window.location.href = intentUrl;
+
+      console.log(intentUrl, "works");
     } else if (isIOS) {
       console.log(pathname);
       window.location.href = `https://hoopla.uz/${locale}${pathname}`;
@@ -57,14 +61,13 @@ export default function DeepLink() {
 
       <Drawer open={open} onOpenChange={setOpen}>
         <DrawerContent className="bg-white rounded-t-3xl border-none max-w-md mx-auto">
-          <DrawerHeader className="pb-6">
+          <DrawerHeader className="pb-8">
             <DrawerTitle className="text-center text-lg font-medium text-gray-900">
-              Выберите, где продолжить
+              {t("deeplink.title")}
             </DrawerTitle>
           </DrawerHeader>
 
-          <div className="px-6 pb-8 space-y-4">
-            {/* Kinopoisk option */}
+          <div className="px-6 pb-6 space-y-4">
             <div className="flex items-center justify-between">
               <div className="flex items-center gap-3">
                 <div className="w-10 h-10 bg-orange-500 rounded-lg flex items-center justify-center relative overflow-hidden">
@@ -78,7 +81,7 @@ export default function DeepLink() {
                   handleOpenApp();
                 }}
               >
-                Перейти
+                {t("deeplink.open-in-app")}
               </Button>
             </div>
           </div>
